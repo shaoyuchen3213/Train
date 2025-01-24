58a50b178bf694cb1a5aea15412aee462da5b3b7
java <chenshaoyu114@gmail.com>
2025-01-24 15:46:13 -0500

    4.14 Successfully integrated the official MyBatis code generator to access the database.

--- member/src/main/java/com/java/member/service/MemberService.java
+++ member/src/main/java/com/java/member/service/MemberService.java
@@ -11,6 +11,6 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
     public int count() {
-        return memberMapper.count();
+        return (int) memberMapper.countByExample(null);
     }
 }
