320dcb59e70757fc0ba4118e6a72cac54d875845
java <chenshaoyu114@gmail.com>
2025-02-02 23:09:59 -0500

    5.6 Add Login Component

--- web/src/main.js
+++ web/src/main.js
@@ -4,7 +4,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'ant-design-vue/dist/reset.css';
-
+import * as Icons from '@ant-design/icons-vue'
 
 const app = createApp(App)
 app.use(Antd).use(store).use(router).mount('#app')
+
+const icons = Icons;
+for( const i in icons) {
+    app.component(i, icons[i]);
+}
